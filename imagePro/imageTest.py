b28957ea41bfc2b8e48a90a4adb8bf3083bfebd2
graceyong1020 <graceyong1020@gmail.com>
2025-01-16 10:18:41 +0900

    환경설정

---
+++ imagePro/imageTest.py
@@ -0,0 +1,8 @@
+import os
+import random
+import torch
+import torch.nn as nn
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader, TensorDataset
+
